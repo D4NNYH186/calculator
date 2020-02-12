c9a02999057c3d14e945d5b3a048cef5a90e0eb2
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-12 11:18:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+
+  state = {
+    counter: 0,
+  }
+
+  mathFunction = () => {
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="calculator">
+        <div>
+          <h1 className="calculator-title">{this.state.counter}</h1>
+        </div>
+        <div className="calculateButtons">
+          <div className="row">
+            <button className="button">clear</button>
+            <button className="button">÷</button>
+            <button className="button">x</button>
+          </div>
+          <div className="row">
+            {/* <button className="button"></button> */}
+            <button className="button">8</button>
+            <button className="button">9</button>
+            <button className="button">-</button>
+          </div>
+          <div className="row">
+          <button className="button">7</button>
+            <button className="button">5</button>
+            <button className="button">+</button>
+            
+            
+          </div>
+          <div className="row">
+            <button className="button">6</button>
+            <button className="button">1</button>
+            <button className="button">2</button>
+          </div>
+          <div className="row">
+            <button className="button">3</button>
+            <button className="button">0</button>
+            <button className="button">4</button>
+          </div>
+          <div className="row2">
+            <button className="clearButton">=</button>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
 export default App;
